5b68de2e3cad48e50afe9f5fc6725771daed1418
burakbagoglu <burakkemalbagoglu45@gmail.com>
2024-02-10 19:55:29 +0300

    database eklendi kayıt olma eklendi

--- main.py
+++ main.py
@@ -7,8 +7,11 @@ from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition
 from noname import *
+import sqlite3
 
-
+vt = sqlite3.connect("database.sql")
+im = vt.cursor()
+im.execute("CREATE TABLE IF NOT EXISTS users ('username','password')")
 
 class Giris(Screen):
     def __init__(self, **kwargs):
@@ -75,14 +78,14 @@ class Kayit(Screen):
         label.color = (0,0,0,1)
         
         logo = Image(source="img/logo.png",pos=(0,270))
-        username_input = TextInput(hint_text='Kullanıcı adı',
+        self.username_input = TextInput(hint_text='Kullanıcı adı',
                                    multiline=False,
                                    size_hint=(None, None),
                                    size=(200, 40),
                                    pos=(150, 400),
                                    background_color=(255,255,255,1))
 
-        password_input = TextInput(hint_text='Şifre',
+        self.password_input = TextInput(hint_text='Şifre',
                                    multiline=False,
                                    size_hint=(None, None),
                                    size=(200, 40),
@@ -96,7 +99,8 @@ class Kayit(Screen):
                         size=(200, 50),
                         pos=(150,300),
                         background_color=(255,255,255,1),
-                        border=(0, 0, 0, 4)
+                        border=(0, 0, 0, 4),
+                        on_press=self.Kayit
                         )
         
         button.color = (0,0,0,1)
@@ -108,19 +112,42 @@ class Kayit(Screen):
         label_hesabin_yokmu.color = (0,0,0,1)
         
         layout.add_widget(main_frame)
-        layout.add_widget(username_input)
-        layout.add_widget(password_input)
+        layout.add_widget(self.username_input)
+        layout.add_widget(self.password_input)
         layout.add_widget(label)
         layout.add_widget(button)
         layout.add_widget(logo)
         layout.add_widget(label_hesabin_yokmu)
         self.add_widget(layout)
 
+    def Kayit(self,instance):
+        print(23)
+        username_value = self.username_input.text
+        password_value = self.password_input.text
+        im.execute("SELECT COUNT(*) FROM users")
+        sutun = im.fetchone()[0]
+        im.execute("SELECT * FROM users")
+        x = False
+        for i in range(sutun):
+            veri1 = im.fetchone()
+            veri2 = str(veri1[0])
+            if username_value == veri2:
+                    x = True
+
+        if len(username_value) > 3 and x == False:
+            hashedsifre = sifrele(password_value)
+            im.execute("INSERT INTO users VALUES (?,?)",(username_value,hashedsifre))
+            vt.commit()
+            self.manager.transition = SlideTransition(direction='right')
+            self.manager.current = 'giris'
+                
+            
+        
     def KayitEkranDegistir(self,instance):
         self.manager.transition = SlideTransition(direction='right')
         self.manager.current = 'giris'
-        
-
+    
+    
 class MyApp(App):
     def build(self):
         # Ekran boyutunu belirleme
